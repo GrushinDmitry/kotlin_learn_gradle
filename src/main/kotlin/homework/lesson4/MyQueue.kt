c04226b78db6bc4074d70a3a2255d3768159e300
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-03-15 19:00:00 +0300

    grouping, rename

--- src/main/kotlin/homework/lesson4/MyQueue.kt
+++ src/main/kotlin/homework/lesson4/MyQueue.kt
@@ -7,10 +7,6 @@ class MyQueue<E : Any>(private val size: Int = 16) {
     private var tail = 0
     private var items = 0
 
-    //Возвращает элемент из головы очереди. Элемент не удаляется.
-    // Если очередь пуста, инициируется исключение NoSuchElementException.
-    fun element(): E = peek() ?: throw NoSuchElementException()
-
 
     //Пытается добавить item в очередь.
     //Возвращает true, если item добавлен, и false в противном случае.
@@ -28,20 +24,19 @@ class MyQueue<E : Any>(private val size: Int = 16) {
         return true
     }
 
-    //Удаляет элемент из головы очереди, возвращая его.
-    //Инициирует исключение NoSuchElementException, если очередь пуста.
-    fun remove(): E = poll() ?: throw NoSuchElementException()
+    //Возвращает элемент из головы очереди. Элемент не удаляется.
+    // Если очередь пуста, инициируется исключение NoSuchElementException.
+    fun element(): E = peek() ?: throw NoSuchElementException()
 
-
-    private fun removeInternal(): E {
-        if (head == size) head = 0
-        items--
-        return queueArray[head++] as E
-    }
 
     //Возвращает элемент из головы очереди.
     //Возвращает null, если очередь пуста. Элемент не удаляется.
     fun peek(): E? = if (isEmpty()) null else queueArray[head] as E
+
+
+    //Удаляет элемент из головы очереди, возвращая его.
+    //Инициирует исключение NoSuchElementException, если очередь пуста.
+    fun remove(): E = poll() ?: throw NoSuchElementException()
 
 
     //Возвращает элемент из головы очереди и удаляет его.
@@ -49,8 +44,15 @@ class MyQueue<E : Any>(private val size: Int = 16) {
     fun poll(): E? =
         when {
             isEmpty() -> null
-            else -> removeInternal()
+            else -> pollInternal()
         }
+
+
+    private fun pollInternal(): E {
+        if (head == size) head = 0
+        items--
+        return queueArray[head++] as E
+    }
 
 
     private fun isEmpty(): Boolean = items == 0
