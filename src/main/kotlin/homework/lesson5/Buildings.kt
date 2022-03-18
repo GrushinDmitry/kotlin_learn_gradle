eaad9bf05d5b5c916544ac8ca96288c73e54c461
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-03-18 21:15:04 +0300

    del issueErrorInputData()

--- src/main/kotlin/homework/lesson5/Buildings.kt
+++ src/main/kotlin/homework/lesson5/Buildings.kt
@@ -6,15 +6,4 @@ class Buildings(
     val area: Int,
     val yearConstruction: Int,
     val price: Int
-) {
-    fun issueErrorInputData() {
-        when {
-            address == "" -> error("Building address not entered")
-            area < 1 -> error("Area cannot be negative or zero")
-            yearConstruction < 1780 -> error("Year of construction cannot be younger than 1780")
-            price < 50000 -> error("Price cannot be less than 50000")
-
-        }
-    }
-
-}
+)
