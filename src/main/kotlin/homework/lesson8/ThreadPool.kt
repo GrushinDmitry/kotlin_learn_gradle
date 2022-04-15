ceb02dcbe5f69f69d03c47c95f82fa9861b0902a
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-04-15 22:15:55 +0300

    add execute, shutdown and other

--- src/main/kotlin/homework/lesson8/ThreadPool.kt
+++ src/main/kotlin/homework/lesson8/ThreadPool.kt
@@ -1,4 +1,48 @@
 package homework.lesson8
 
-class ThreadPool {
+import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.LinkedBlockingQueue
+
+class ThreadPool(size: Int) : Executor {
+
+    private val tasks = LinkedBlockingQueue<Runnable>()
+    private val threads = LinkedList<WorkerThread>()
+
+    @Volatile
+    private var isStarted = false
+
+    init {
+        if (size !in 1..maxSize) throw IllegalArgumentException("The size not in the range of 1 to $maxSize")
+        threads.forEach { it.start() }
+    }
+
+    override fun execute(command: Runnable) {
+        if (!isStarted) throw IllegalThreadStateException("Thread pool stopped")
+        synchronized(tasks) {
+            tasks.add(command)
+            (tasks as Object).notify()
+        }
+    }
+
+    fun shutdown() {
+        isStarted = false
+        synchronized(tasks) {
+            (tasks as Object).notifyAll()
+        }
+    }
+
+    private class WorkerThread : Thread() {
+
+        override fun run() {
+
+            while (true) {
+
+            }
+        }
+
+    }
+
 }
+
+private const val maxSize = 8
