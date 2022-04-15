92ae4f2c39d8465e56711e4851c1c65fd4d4f963
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-04-15 19:34:03 +0300

    init

---
+++ src/main/kotlin/homework/lesson8/ThreadPool.kt
@@ -0,0 +1,4 @@
+package homework.lesson8
+
+class ThreadPool {
+}
---
+++ src/test/kotlin/homework/lesson8/ThreadPoolTest.kt
@@ -0,0 +1,15 @@
+package homework.lesson8
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+internal class ThreadPoolTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+}
