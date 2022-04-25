2c3031d3854fae8291c1e530c940c9ba2f5af66b
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-04-25 23:47:05 +0300

    fix remarks

--- src/test/kotlin/homework/lesson8/ThreadPoolTest.kt
+++ src/test/kotlin/homework/lesson8/ThreadPoolTest.kt
@@ -5,13 +5,17 @@ import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.mockk.clearAllMocks
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import java.lang.Thread.sleep
 import kotlin.random.Random
 
 class ThreadPoolTest : FeatureSpec() {
 
-    private val mockJob = mockk<() -> Runnable>(relaxed = true)
+    private val mockJob = mockk<Runnable> {
+        every { run() } answers { sleep(200) }
+    }
 
 
     override fun afterAny(testCase: TestCase, result: TestResult) {
@@ -31,33 +35,31 @@ class ThreadPoolTest : FeatureSpec() {
                 val threadPool = ThreadPool(sizeThreadPoolRandom)
 
                 threadPool.shutdown()
+                shouldThrow<IllegalStateException> { threadPool.execute(mockJob) }
 
-                shouldThrow<IllegalStateException> { threadPool.execute { mockJob() } }
-                verify(exactly = 0) { mockJob() }
+                verify(exactly = 0) { mockJob.run() }
             }
             scenario("job executed before shutdown") {
-                val sizeThreadPoolRandom = 1 + Random.nextInt(7)
+                val sizeThreadPoolRandom = 1//1 + Random.nextInt(7)
                 val threadPool = ThreadPool(sizeThreadPoolRandom)
 
-                threadPool.execute { mockJob() }
+                threadPool.execute(mockJob)
+                sleep(500)
                 threadPool.shutdown()
-
-                verify(exactly = 1) { mockJob() }
+                verify(exactly = 1) { mockJob.run() }
             }
             scenario("success work") {
-                val sizeThreadPoolRandom = 1 + Random.nextInt(7)
-                val valueTasks = Random.nextInt(15)
+                val sizeThreadPoolRandom = 8
+                val valueTasks = 16
                 val threadPool = ThreadPool(sizeThreadPoolRandom)
 
                 repeat(valueTasks) {
-                    threadPool.execute { mockJob() }
+                    threadPool.execute(mockJob)
                 }
 
-                verify(exactly = valueTasks) { mockJob() }
-                threadPool.shutdown()
+                verify(exactly = valueTasks) { mockJob.run() }
             }
         }
     }
 }
-
 
